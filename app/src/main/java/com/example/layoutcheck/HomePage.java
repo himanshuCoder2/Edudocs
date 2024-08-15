a47340327e1b25bd8df0d712956b581a870c8f5f
Dhiraj <dhirajmandal1216@gmail.com>
2024-08-15 14:30:05 +0530

    I Add New Activities 1.Notes 2.Document 3.Profile and also write intent code for this activities on the HomePage ,also provide internet connection AndroidManifest.xml file, also change on SplashScreen.java file i change the layout and arrange the image and text on proper order.

--- app/src/main/java/com/example/layoutcheck/HomePage.java
+++ app/src/main/java/com/example/layoutcheck/HomePage.java
@@ -1,6 +1,9 @@
 package com.example.layoutcheck;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomePage extends AppCompatActivity {
-
+LinearLayout home,notes,documents,profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,44 @@ public class HomePage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        home=findViewById(R.id.tab_home);
+        notes=findViewById(R.id.tab_notes);
+        documents=findViewById(R.id.tab_documents);
+        profile=findViewById(R.id.tab_profile);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
+
+
+        notes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, Notes.class);
+                startActivity(intent);
+            }
+        });
+
+        documents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, S_Documents.class);
+                startActivity(intent);
+            }
+        });
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, S_Profile.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/layoutcheck/Notes.java
+++ app/src/main/java/com/example/layoutcheck/Notes.java
@@ -1,8 +1,6 @@
 package com.example.layoutcheck;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SplashScreen extends AppCompatActivity {
-
+public class Notes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash_screen);
+        setContentView(R.layout.activity_notes);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run(){
-                startActivity(new Intent(SplashScreen.this, MainActivity.class));
-            }
-        },2000);
     }
 }
