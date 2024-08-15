39f9f23ca2e8464e3ef8b745e949d622eb6eb9f4
himanshuCoder <himanshu.bijwar.bca@ghriet.raisoni.net>
2024-08-15 23:43:36 +0530

    1: Add in Progress bar in the splashscreen.xml file write it on the code . 2: And Deleted my side comactivity xml file in which is present . 3: (Pending work) We have to add a code for the progress bar in the SplashScreen.java file.. 4 : along with pushing the changes it will be changes the 5 files including just as 1 activity_comactivity.xml 2 activity_splash_screen.xml 3 AndroidManifest.xml 4 SplashScreen.java 5 strings.xml

--- app/src/main/java/com/example/layoutcheck/SplashScreen.java
+++ app/src/main/java/com/example/layoutcheck/SplashScreen.java
@@ -21,6 +21,7 @@ public class SplashScreen extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+
             return insets;
         });
         new Handler().postDelayed(new Runnable(){
